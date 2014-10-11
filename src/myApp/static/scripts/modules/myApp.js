345e819205981bb143ab8c4970f6cb391c15f48c
Kevin Chan <chessmasterhong@yahoo.com>
2014-10-11 01:30:13 -0400

    Added AngularUI Router. Added basic home controller.

--- src/myApp/static/scripts/modules/myApp.js
+++ src/myApp/static/scripts/modules/myApp.js
@@ -1,1 +1,12 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', [
+    'ui.router'
+])
+.config(function($stateProvider) {
+    'use strict';
+
+    $stateProvider
+        .state('home', {
+            url: '',
+            templateUrl: 'static/scripts/modules/home/home.partial.html'
+        });
+});
