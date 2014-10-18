6623eba47acf64220d17946c9b1788f4a17de954
Kevin Chan <chessmasterhong@yahoo.com>
2014-10-18 00:54:17 -0400

    Added basic ionic sidebar.

--- src/myApp/static/scripts/config/routes.js
+++ src/myApp/static/scripts/config/routes.js
@@ -3,11 +3,15 @@ define([
 ], function(app) {
     'use strict';
 
-    return app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider) {
+    return app.config(function($stateProvider) {
         $stateProvider
-            .state('home', {
+            .state('main', {
+                abstract: 'true',
+                templateUrl: 'static/scripts/modules/common/sidebar.partial.html'
+            })
+            .state('main.home', {
                 url: '',
                 templateUrl: 'static/scripts/modules/home/home.partial.html'
             });
-    }]);
+    });
 });
