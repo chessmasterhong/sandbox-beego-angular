6d9e92b4fc345197d4744259bd5516a690ccbb20
Kevin Chan <chessmasterhong@yahoo.com>
2014-10-21 00:44:45 -0400

    Removed window.cordova checks. Minor JSHint fix to scope.

--- src/myApp/static/scripts/myApp.js
+++ src/myApp/static/scripts/myApp.js
@@ -13,12 +13,8 @@ define([
     ])
     .run(function($ionicPlatform) {
         $ionicPlatform.ready(function() {
-            if(window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
-
             if(window.StatusBar) {
-                StatusBar.styleDefault();
+                window.StatusBar.styleDefault();
             }
         });
     });
